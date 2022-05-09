5b238d2b752e51cead0753ab43adab4e09131787
Jerry Nyoike <jngugi31@gmail.com>
2022-05-09 06:40:32 +0300

    fix: Add functions to run on windows for portability

--- repl.c
+++ repl.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-#include <editline/history.h>
+#ifdef _WIN32
+#include <string.h>
 
 /* Declare a buffer for user input of size 2048. */
-char input[2048];
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  /* Output the prompt. */
+  fputs(prompt, stdout);
+
+  /* Get the input and store it in the input buffer. */
+  fgets(input, 2048, stdin);
+
+  /* Makea copy of the input and return it. */
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Add a fake add_history function. */
+void add_history(char* unused);
+
+/* Otherwise add the required headers. */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv){
   /* Print Version and exit information. */
@@ -16,10 +39,6 @@ int main(int argc, char** argv){
   while (1) {
     /* Output the prompt */
     char* input = readline("Lispy> ");
-    /* fputs("Lispy> ", stdout); */
-
-    /* Read a line of user input of max size 2048 */
-    /* fgets(input, 2048, stdin); */
 
     /* Add line to history. */
     add_history(input);
@@ -27,7 +46,7 @@ int main(int argc, char** argv){
     /* Echo input back to user */
     printf("No you are a %s\n", input);
 
-    /* Free retrieved input.  */
+    /* Free retrieved input. */
     free(input);
   }
 
